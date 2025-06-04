5e05b97ab31b83085748c3870a45c70a2211432d
gojun16 <fernando.schlichting@escola.pr.gov.br>
2025-06-04 11:57:44 -0300

    Create databasea.py

---
+++ databasea.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS produtos(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT NOT NULL,lote INTEGER NOT NULL,nota_fiscal TEXT,validade DATE NOT NULL)")
