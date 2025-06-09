e36071d880e32c3590a1fbc1644bc8493d0a61ed
gojun16 <fernando.schlichting@escola.pr.gov.br>
2025-06-09 12:26:47 -0300

    Update databasea.py

--- databasea.py
+++ databasea.py
@@ -3,5 +3,4 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS produtos(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT NOT NULL,lote INTEGER NOT NULL,nota_fiscal TEXT,validade DATE NOT NULL, quantidade INTEGER NOT NULL)")
-
+cursor.execute("CREATE TABLE IF NOT EXISTS produtos(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT NOT NULL,lote INTEGER,nota_fiscal TEXT,validade DATE, quantidade INTEGER NOT NULL)")
